f14d92f714ddd026f497f565f356a52e182ae5ef
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-10-31 03:48:37 +0600

    setup cargokit

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.rust_file_example
+package com.example.example
 
 import io.flutter.embedding.android.FlutterActivity
 
--- macos/Classes/rust_file.c
+++ macos/Classes/rust_file.c
@@ -1,3 +1,1 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../rust_file.podspec for more information.
-#include "../../src/rust_file.c"
+// This is an empty file to force CocoaPods to create a framework.
