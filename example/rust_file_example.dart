bd0c4168e6ec095d0c3d3adadf35ee0135c8cfa9
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-10-31 01:56:08 +0600

    project create

---
+++ example/rust_file_example.dart
@@ -0,0 +1,6 @@
+import 'package:rust_file/rust_file.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/rust_file_test.dart
@@ -0,0 +1,16 @@
+import 'package:rust_file/rust_file.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
